d34bb27dbf85b75e83271e491bfce00ee33f66af
bwchangster93 <b.changster93@gmail.com>
2023-11-29 14:32:53 -0800

    helloworld script

---
+++ helloworld.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Script Name : Helloworld
+# Author: Benjamin Chang
+# date of latest revision: 11/28/2023
+# Purpose: Create a script that prints a string to the console
+
+# Declaration of variables
+myvariable='i am working perfectly'
+# Declaration of functions
+echo $myvariable
+
+# Main
+
+echo
+
+# End
